4778c675e3874defc7b20e7e090160359de4f03a
jayce GESLIN GRANDIERES <jayce.grandieres@laposte.net>
2017-09-08 16:38:18 +0200

    (login non fonctionnel, on peut se loger sans se connecté et le register ne fonctionne plus)

--- data/resources/views/menu.blade.php
+++ data/resources/views/menu.blade.php
@@ -26,7 +26,7 @@
                            onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
                             Se Déconnecter</a>
-                        <form id="logout-form" action="{{ url('/logout') }}" method="GET" style="display: none;">
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                             {{ csrf_field() }}
                         </form>
                     </form>
