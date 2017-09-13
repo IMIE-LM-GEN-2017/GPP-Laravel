e2724542684cdb69d724a8a0d77a9c51e84cdcb3
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-09-13 11:36:12 +0200

    Somewhat fixed routes

--- data/routes/web.php
+++ data/routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-// Accueil et Login
-Route::get('/', function () {
-    return view('index');
-})->name('index');
 
-// Logout
-Route::get('/logout', 'Auth\LoginController@logout');
+// Login et récupération de mdp
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('home');
+//Route::post('/', 'Auth\LoginController@login')->name('login');
+//Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+//Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+//Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('passwordreset');
+
+
+Route::get('/menu', 'HomeController@index')->name('menu');
 
 // Le Contact
 Route::post('/contact', 'ContactController@send')->name('SendContact');
@@ -22,45 +27,47 @@ Route::get('/todolists/create', 'TodolistController@create')->name('TodolistCrea
 Route::post('/todolists/{id}/update', 'TodolistController@update')->name('TodolistUpdate');
 Route::get('/todolists/{id}/edit', 'TodolistController@edit')->name('TodolistEdit');
 
+Route::get('/users/create', 'Auth\RegisterController@showRegistrationForm');
+Route::post('/users/save', 'Auth\RegisterController@register')->name('register');
+
 // Admin
 Route::group(['prefix' => 'admin', 'middleware' => 'can:access-admin'], function () {
-    Route::post('/register', 'AdminRegisterController@register')->name('AdminRegister');
 
-    Route::get('/todolists', 'AdminTodolistController@index')->name('AdminTodolistIndex');
-    Route::get('/todolists/{id}', 'AdminTodolistController@show')->name('AdminTodolistShow');
-    Route::get('/todolists/create', 'AdminTodolistController@create')->name('AdminTodolistCreate');
-    Route::post('/todolists/{id}/update', 'AdminTodolistController@update')->name('AdminTodolistUpdate');
-    Route::get('/todolists/{id}/edit', 'AdminTodolistController@edit')->name('AdminTodolistEdit');
-    Route::get('/todolists/{id}/destroy', 'AdminTodolistController@destroy')->name('AdminTodolistDestroy');
+    Route::get('/todolists', 'Admin\TodolistController@index')->name('AdminTodolistIndex');
+    Route::get('/todolists/{id}', 'Admin\TodolistController@show')->name('AdminTodolistShow');
+    Route::get('/todolists/create', 'Admin\TodolistController@create')->name('AdminTodolistCreate');
+    Route::post('/todolists/{id}/update', 'Admin\TodolistController@update')->name('AdminTodolistUpdate');
+    Route::get('/todolists/{id}/edit', 'Admin\TodolistController@edit')->name('AdminTodolistEdit');
+    Route::get('/todolists/{id}/destroy', 'Admin\TodolistController@destroy')->name('AdminTodolistDestroy');
     Route::post('/todolists/store', 'Admin\TodolistController@store')->name('AdminTodolistStore');
 
-    Route::get('/presences', 'AdminPresenceController@index')->name('AdminPresenceIndex');
-    Route::get('/presences/{id}', 'AdminPresenceController@show')->name('AdminPresenceShow');
-    Route::get('/presences/create', 'AdminPresenceController@create')->name('AdminPresenceCreate');
-    Route::post('/presences/{id}/update', 'AdminPresenceController@update')->name('AdminPresenceUpdate');
-    Route::get('/presences/{id}/edit', 'AdminPresenceController@edit')->name('AdminPresenceEdit');
-    Route::get('/presences/{id}/destroy', 'AdminPresenceController@destroy')->name('AdminPresenceDestroy');
+    Route::get('/presences', 'Admin\PresenceController@index')->name('AdminPresenceIndex');
+    Route::get('/presences/{id}', 'Admin\PresenceController@show')->name('AdminPresenceShow');
+    Route::get('/presences/create', 'Admin\PresenceController@create')->name('AdminPresenceCreate');
+    Route::post('/presences/{id}/update', 'Admin\PresenceController@update')->name('AdminPresenceUpdate');
+    Route::get('/presences/{id}/edit', 'Admin\PresenceController@edit')->name('AdminPresenceEdit');
+    Route::get('/presences/{id}/destroy', 'Admin\PresenceController@destroy')->name('AdminPresenceDestroy');
     Route::post('/presences/store', 'Admin\PresenceController@store')->name('AdminPresenceStore');
 
-    Route::get('/users', 'AdminUserController@index')->name('AdminUserIndex');
-    Route::get('/users/{id}', 'AdminUserController@show')->name('AdminUserShow');
-    Route::get('/users/create', 'AdminUserController@create')->name('AdminUserCreate');
-    Route::post('/users/{id}/update', 'AdminUserController@update')->name('AdminUserUpdate');
-    Route::get('/users/{id}/edit', 'AdminUserController@edit')->name('AdminUserEdit');
-    Route::get('/users/{id}/destroy', 'AdminUserController@destroy')->name('AdminUserDestroy');
-    Route::post('/users/store', 'Admin\UsersController@store')->name('AdminUserStore');
+    Route::get('/users', 'Admin\UserController@index')->name('AdminUserIndex');
+    Route::get('/users/{id}', 'Admin\UserController@show')->name('AdminUserShow');
+//    Route::get('/users/create', 'Admin\UserController@create')->name('AdminUserCreate');
+//    Route::post('/users/store', 'Admin\UserController@store')->name('AdminUserStore');
+    Route::get('/users/{id}/edit', 'Admin\UserController@edit')->name('AdminUserEdit');
+    Route::post('/users/{id}/update', 'Admin\UserController@update')->name('AdminUserUpdate');
+    Route::get('/users/{id}/destroy', 'Admin\UserController@destroy')->name('AdminUserDestroy');
 
-    Route::get('/plannings', 'AdminPlanningController@index')->name('AdminPlanningIndex');
-    Route::get('/plannings/{id}', 'AdminPlanningController@show')->name('AdminPlanningShow');
-    Route::get('/plannings/create', 'AdminPlanningController@create')->name('AdminPlanningCreate');
-    Route::post('/plannings/{id}/update', 'AdminPlanningController@update')->name('AdminPlanningUpdate');
-    Route::get('/plannings/{id}/edit', 'AdminPlanningController@edit')->name('AdminPlanningEdit');
-    Route::get('/plannings/{id}/destroy', 'AdminPlanningController@destroy')->name('AdminPlanningDestroy');
+//    Route::get('/admin/users/create', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//    Route::post('/admin/users/save', 'Auth\RegisterController@register');
+
+    Route::get('/plannings', 'Admin\PlanningController@index')->name('AdminPlanningIndex');
+    Route::get('/plannings/{id}', 'Admin\PlanningController@show')->name('AdminPlanningShow');
+    Route::get('/plannings/create', 'Admin\PlanningController@create')->name('AdminPlanningCreate');
     Route::post('/plannings/store', 'Admin\PlanningController@store')->name('AdminPlanningStore');
+    Route::get('/plannings/{id}/edit', 'Admin\PlanningController@edit')->name('AdminPlanningEdit');
+    Route::post('/plannings/{id}/update', 'Admin\PlanningController@update')->name('AdminPlanningUpdate');
+    Route::get('/plannings/{id}/destroy', 'Admin\PlanningController@destroy')->name('AdminPlanningDestroy');
 
 });
 
-// Ajout par php artisan make:auth pour le login et enregistrement
 Auth::routes();
-
-Route::get('/menu', 'HomeController@index')->name('menu');
